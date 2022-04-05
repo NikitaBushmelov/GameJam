33ed7a25f849c4b68f141b1e4547b78213fe44d9
NikitaBushmelov <bushmelovnikita@gmail.com>
2022-04-05 12:47:09 +0200

    main

---
+++ GameJam2/Assets/Scripts/PlayerController1.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController1 : MonoBehaviour
+{
+    bool moveLeft;
+    bool moveRight;
+    Rigidbody2D rb2d;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveLeft = Input.GetKey(KeyCode.A);
+        moveRight= Input.GetKey(KeyCode.D);
+    }
+    private void FixedUpdate()
+    {
+        if (moveLeft) {
+            rb2d.AddForce(new Vector2(-speed,0f));
+        }
+        if (moveRight)
+        {
+            rb2d.AddForce(new Vector2(speed, 0f));
+        }
+    }
+}
---
+++ GameJam2/Assets/Scripts/PlayerController2.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour
+{
+    bool moveLeft;
+    bool moveRight;
+    Rigidbody2D rb2d;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveLeft = Input.GetKey(KeyCode.LeftArrow);
+        moveRight = Input.GetKey(KeyCode.RightArrow);
+    }
+    private void FixedUpdate()
+    {
+        if (moveLeft)
+        {
+            rb2d.AddForce(new Vector2(-speed, 0f));
+        }
+        if (moveRight)
+        {
+            rb2d.AddForce(new Vector2(speed, 0f));
+        }
+    }
+}
